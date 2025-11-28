7c96eeb6ed8166e081290752fb1fed84b855f2df
Vijeesh <vijeesh82@gmail.com>
2025-11-28 18:27:50 +0530

    chore: remove dead code

--- src/frontend/src/components/Dashboard.vue
+++ src/frontend/src/components/Dashboard.vue
@@ -280,14 +280,6 @@ const onTimerStop = () => {
 }
 
 const saveCategoryChanges = () => {
-    // if (isLocked.value) {
-    //     logMessage('Locked! Cannot change categories.')
-    //     return
-    // }
-    // if (hasCardsInDisabledCategories || !isCategorySelectionValid) return
-
-    // TODO: PAYLOAD SIZE VALIDATION ?
-
     const enabledCols: BoardColumn[] = mergedCategories.value.filter(c => c.enabled === true)
         .map(c => ({
             id: c.id,
@@ -301,8 +293,8 @@ const saveCategoryChanges = () => {
         logMessage('Columns unchanged. Not dispatching.')
         return
     }
+    
     if (exceedsEventRequestMaxSize<ColumnsChangeEvent>("colreset", { by: user, grp: board, columns: enabledCols })) {
-        // TODO: Create new key to have a different message
         toast.error(t('common.contentOverloadError'))
         return
     }
@@ -1203,18 +1195,6 @@ onUnmounted(() => {
                         d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
                 </svg>
             </div>
-            <!-- TODO: Translations -->
-            <!-- Edit Columns -->
-            <!-- <div title="Edit columns">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor"
-                    stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
-                    class="w-8 h-8 mx-auto mb-4 cursor-pointer" v-if="isOwner" @click="openColumnEditDialog">
-                    <path d="M12 3H5a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7" />
-                    <path
-                        d="M18.375 2.625a1 1 0 0 1 3 3l-9.013 9.014a2 2 0 0 1-.853.505l-2.873.84a.5.5 0 0 1-.62-.62l.84-2.873a2 2 0 0 1 .506-.852z" />
-                </svg>
-            </div> -->
-
             <a href="https://github.com/vijeeshr/quickretro" target="_blank" rel="noopener noreferrer">
                 <svg viewBox="0 0 24 24" aria-hidden="true" class="h-8 w-8 mx-auto mb-4 fill-slate-100">
                     <path fill-rule="evenodd" clip-rule="evenodd"
